bc8fb382089fd73a6dce8e4452cd69fde84da754
DESKTOP-J4CVPG5\Antoine <antoine.aubree@protonmail.com>
2021-01-18 16:41:46 +0100

    fix users.router

--- app/router/users.router.js
+++ app/router/users.router.js
@@ -5,11 +5,12 @@ const usersController = require('../controller/users.controller.js');
 router.post('/register', usersController.register);
 router.post('/login', usersController.login);
 
-router.post('/me', usersController.createSudentOrTeacher);
-router.post('/me/modify-email', usersController.updateEmail);
-router.post('/me/modify-password', usersController.updatePassword);
+router.put('/me/modify-email', usersController.updateEmail);
+router.put('/me/modify-password', usersController.updatePassword);
 router.delete('/me/delete', usersController.remove);
-router.put('/me/:id', usersController.updateStudentOrTeacher);
 router.get('/me', usersController.getMe);
+
+router.post('/me', usersController.createSudentOrTeacher);
+router.put('/me/:id', usersController.updateStudentOrTeacher);
 
 module.exports = router;
